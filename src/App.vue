b011f9de11d1ff43aa5b39edf43efbe06e799de5
Kennedy <kennedymessias.vb@gmail.com>
2020-03-01 13:10:25 -0300

    initing home

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
+  <div class="main">
+    <headerApp></headerApp>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import Header from './components/Header';
 
 export default {
   name: 'App',
   components: {
-  }
-}
+    headerApp: Header,
+  },
+};
 </script>
 
-<style>
-</style>
+<style></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+    <p>Home</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home ',
+};
+</script>
+
+<style></style>
