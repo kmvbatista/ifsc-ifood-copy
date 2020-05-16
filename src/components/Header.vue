84e3d94b24134bf0dc45d184188e4df1776ac642
Kennedy <kennedymessias.vb@gmail.com>
2020-05-16 13:46:36 -0300

    header options outbones

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="header">
     <div class="logo-box">
-      <img src="@/assets/images/ifood-logo.png" alt="logo" />
+      <img class="logo" src="@/assets/images/ifood-logo.png" alt="logo" />
     </div>
-    <p>busque o que você quer</p>
-    <p>entregar em</p>
+    <div class="search">
+      <img src="icons/search.svg" class="icon" alt="localização" >
+    <input type="text" placeholder="Busque por item ou loja" class="search-input">
+    </div>
+    <div class="location">
+      <p>ENTREGAR EM</p>
+    <img src="icons/location.svg" class="icon" alt="localização" >
+    </div>
     <div class="options-box">
-      <div class="tt">
-        <p>promoçoes</p>
+      <div class="header-option">
+        <img src="icons/commerce.svg" class="icon" alt="promoções" >
+        <p class="header-option-text">Promoçoes</p>
       </div>
-      <div class="tt">
-        <p>perfil</p>
+      <div class="header-option">
+        <img src="icons/door.svg" class="icon" alt="entrar" >
+        <p class="header-option-text">Entrar</p>
       </div>
-      <div class="tt">
-        <p>sacola</p>
+      <div class="header-option">
+        <img src="icons/bag.svg" class="icon" alt="sacola" >
+        <p class="header-option-text">Sacola</p>
       </div>
     </div>
   </div>
@@ -25,16 +34,26 @@ export default {
 };
 </script>
 
-<style lang="css" scoped>
+<style scoped>
 .header {
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-between;
+}
+.header-option {
+  display: flex;
+}
+.header-option > img {
+  margin-right: 3px;
 }
 .options-box {
   display: flex;
   justify-content: space-around;
 }
+.options-box > :not(:last-child) {
+  margin-right: 50px;
+}
+
 .logo-box {
   height: 100px;
   width: 100px;
@@ -43,8 +62,12 @@ export default {
 .tt {
   display: block;
 }
+.icon {
+  width: 20px;
+  height: 20px;
+}
 
-img {
+.logo {
   max-width: 100%;
   max-height: 100%;
   top: 50%;
