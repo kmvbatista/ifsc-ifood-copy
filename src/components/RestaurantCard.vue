905a9aa67a44615595a4682adf2930a325f5969f
Kennedy <kennedymessias.vb@gmail.com>
2020-05-16 21:20:09 -0300

    refactoring restaurantCard

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="card">
+  <div class="card row">
     <div class="restaurant-photo" :style="`background-image: url(${restaurant.photo})`"></div>
     <div class="spacer"></div>
     <div class="card-infos column">
       <strong>{{restaurant.name}}</strong>
       <div class="row">
-        <img src="icons/star.svt" alt="avaliação" class="rating-icon" />
+        <img src="icons/star.svg" alt="avaliação" class="rating-icon" />
         <p class="rating">{{restaurant.rating}}</p>
-        <p>* {{restaurant.category}} * {{restaurant.distance}}km</p>
+        <p>• {{restaurant.category}} • {{restaurant.distance}}km</p>
       </div>
       <div class="row">
-        <p>{{restaurant.deliveryTime}} * ''</p>
-        <p v-if="restaurant.deliveryPrice">{{restaurant.deliveryPrice}}</p>
+        <p>{{restaurant.deliveryTime}} •</p>
+        <p v-if="restaurant.deliveryPrice">{{'R$ ' + restaurant.deliveryPrice}}</p>
         <p v-else class="free-delivery">Entrega grátis</p>
       </div>
     </div>
@@ -25,23 +25,31 @@ export default {
 </script>
 
 <style scoped>
-:root {
-  --card-height: 70px;
+* {
+  --card-height: 120px;
 }
 .restaurant-photo {
   background-size: cover;
-  width: calc(var(--card-heigth) * 80%);
-  height: calc(var(--card-heigth) * 80%);
+  width: calc(var(--card-height) * 0.8);
+  height: calc(var(--card-height) * 0.8);
 }
 .card {
-  display: flex;
-  padding: calc(var(--card-height) * 10%);
+  width: 100%;
+  max-width: 500px;
+  height: var(--card-height);
+  padding: calc(var(--card-height) * 0.15);
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
 }
-
+p {
+  margin-right: 5px;
+  font-size: 0.95em;
+}
 .spacer {
   width: 1px;
   height: 100%;
-  margin: 0 10px;
+  margin: 0 20px;
+  background-color: #f2f2f2;
 }
 
 .card-infos {
@@ -52,9 +60,9 @@ export default {
   color: #e7a74e;
 }
 .rating-icon {
-  width: 7px;
-  height: 7px;
-  margin-right: 2px;
+  height: 13px;
+  width: 13px;
+  margin: auto 3px;
 }
 .free-delivery {
   color: #50a773;
